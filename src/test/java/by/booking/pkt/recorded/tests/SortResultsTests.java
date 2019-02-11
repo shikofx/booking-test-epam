4da84d3c5a2027d1a42024b8294e12ec695bda87
Dzmitry Parkheichuk <shikofx@users.noreply.github.com>
2019-02-11 19:10:38 +0300

    Fluent + PageObject 1

---
+++ src/test/java/by/booking/pkt/recorded/tests/SortResultsTests.java
@@ -0,0 +1,58 @@
+package by.booking.pkt.recorded.tests;
+
+import by.booking.pkt.recorded.model.SearchData;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+
+public class SortResultsTests extends TBase {
+  @Test
+  public void testSortByDistance() {
+    app.forSearch().fillForm(new SearchData().withCurrency("RUB").withPlace("Минск").
+            withInDate("2019-03-24").withOutDate("2019-04-03").
+            withRooms(5).withAdults(2).withChildren(4));
+    app.forSearch().initSearch();
+    //app.getFilter().selectOnlyAvailable();
+    app.results().initSortByDistance();
+    Assertion assertion = new Assertion();
+    List<WebElement> searchResults = app.results().getAll();
+    List<WebElement> availabelResults = app.results().getOnlyAvailable(searchResults);
+    for (int i = 1; i < availabelResults.size(); i++) {
+      String currentHotelName = app.results().getHotelName(availabelResults.get(i));
+      int currentDistance = app.results().distanceToDest(availabelResults.get(i));
+      String previousHotelName = app.results().getHotelName(availabelResults.get(i - 1));
+      int previousDistance = app.results().distanceToDest(availabelResults.get(i - 1));
+      assertion.assertTrue(currentDistance >= previousDistance,
+              "Unvalid sort by distance.\n" +
+                      "The place " + currentHotelName + " with distance " + currentDistance +
+                      " must be higher than " + previousHotelName + " with distance " + previousDistance);
+    }
+  }
+
+  @Test
+  public void sortByPrice() {
+    SoftAssert softAssert = new SoftAssert();
+    app.forSearch().fillForm(new SearchData().withCurrency("RUB").withPlace("Минск").
+            withInDate("2019-03-24").withOutDate("2019-04-03").
+            withRooms(5).withAdults(2).withChildren(4));
+    app.forSearch().initSearch();
+    // app.getFilter().selectOnlyAvailable();
+    app.results().initSortByPrice();
+    List<WebElement> searchResults = app.results().getAll();
+    List<WebElement> availabelResults = app.results().getOnlyAvailable(searchResults);
+    for (int i = 1; i < availabelResults.size(); i++) {
+      String currentHotel = app.results().getHotelName(availabelResults.get(i));
+      int currentPrice = app.results().totalPrice(availabelResults.get(i));
+      String previousHotel = app.results().getHotelName(availabelResults.get(i - 1));
+      int previousPrice = app.results().totalPrice(availabelResults.get(i - 1));
+      Assertion assertion = new Assertion();
+      assertion.assertTrue(currentPrice >= previousPrice,
+              "Unvalid sort by total price.\n" +
+                      "The place '" + currentHotel + "' with total price " + currentPrice +
+                      " must be higher than '" + previousHotel + "' with total price " + previousPrice);
+    }
+  }
+}
